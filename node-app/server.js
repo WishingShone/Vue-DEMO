baa77879948968186b7cfbcaf726352e7ca10a94
shone <syx19921022@gmail.com>
2019-07-29 13:25:25 +0800

    1、增加登录的token验证 2、增加身份的验证

--- node-app/server.js
+++ node-app/server.js
@@ -1,9 +1,47 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+//引入token验证模块
+const passport = require("passport");
+
 const App = express();
+
+//引入users.js
+const users = require("./routers/api/users");
+
+//mongodb配置
+const db = require("./config/keys").mongoURI;
+
+//使用body-parser中间件
+App.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
+App.use(bodyParser.json());
+
+//连接到mobgodb
+mongoose
+  .connect(db)
+  .then(() => {
+    console.log("mongodb已连接");
+  })
+  .catch(err => {
+    console.log(err);
+  });
 //设置根路由
 App.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+//passport初始化
+App.use(passport.initialize());
+//传递passport到passport.js来使用
+require("./config/passport")(passport);
+
+//使用router
+App.use("/api/users", users);
+
 const port = process.env.PORT || 5000;
 
 App.listen(port, () => {
